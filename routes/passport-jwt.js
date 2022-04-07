e479da61fa7ebbcd95dde755e300e420e241446a
muanziri <munaziribienaime@yahoo.com>
2022-04-07 16:52:40 +0000

    jer

--- routes/passport-jwt.js
+++ routes/passport-jwt.js
@@ -9,15 +9,17 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.JWTSecret;
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     console.log(jwt_payload)
-    buyerClient.findOne({id: jwt_payload.id}, function(err, user) {
-        if (err) {
-            return done(err, false);
-        }
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-            // or you could create a new account
-        }
+    buyerClient.findOne({id: jwt_payload.id}).then((user)=>{
+    
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+                // or you could create a new account
+            }
+    
     });
 }));
